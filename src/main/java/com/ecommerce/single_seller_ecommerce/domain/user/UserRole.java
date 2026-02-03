98c291481181b1381f3427901ba93c495ec9ddc4
sangyong <bagsangyong34@gmail.com>
2026-02-03 17:32:24 +0900

    feat(user): add user entity and role/status enums

---
+++ src/main/java/com/ecommerce/single_seller_ecommerce/domain/user/UserRole.java
@@ -0,0 +1,7 @@
+package com.ecommerce.single_seller_ecommerce.domain.user;
+
+public enum UserRole {
+    CUSTOMER, // 구매자
+    SELLER, // 판매자
+    ADMIN // 관리자
+}
---
+++ src/main/java/com/ecommerce/single_seller_ecommerce/domain/user/UserStatus.java
@@ -0,0 +1,7 @@
+package com.ecommerce.single_seller_ecommerce.domain.user;
+
+public enum UserStatus {
+    ACTIVE, // 정상 이용 중
+    SUSPENDED, // 일시 정지
+    WITHDRAWN // 탈퇴
+}
