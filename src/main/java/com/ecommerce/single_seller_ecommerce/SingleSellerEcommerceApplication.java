b38f955f1c49f28a41f1a43f6e8b58f5b2fe71d7
sangyong <bagsangyong34@gmail.com>
2026-02-03 17:01:15 +0900

    Initial commit - project setup

---
+++ src/main/java/com/ecommerce/single_seller_ecommerce/SingleSellerEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.single_seller_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSellerEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSellerEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/single_seller_ecommerce/SingleSellerEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.single_seller_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSellerEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
